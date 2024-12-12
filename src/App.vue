d9d7ca772867dcd35872e794b45dcbff454b3fea
culturetogo <m.mueller@culture-to-go.de>
2024-12-12 11:33:32 +0100

    first implementation of data fetching from Omeka S instance

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
@@ -32,10 +19,7 @@ header {
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+
 }
 
 nav a.router-link-exact-active {
@@ -57,29 +41,6 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,49 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, computed, onMounted } from 'vue';
+
+const itemsData = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+const items = computed(() => {
+  return itemsData.value
+  .filter(item => item['o:resource_class']['o:id'] === 32)
+});
+
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://omeka-s-t1.berlin-university-collections.de/api/items');
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    itemsData.value = await response.json();
+  } catch (err) {
+    error.value = err instanceof Error ? err.message : 'Unknown error';
+  } finally {
+    loading.value = false;
+  }
+});
+/*
+
+
+*/
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Provenienzforschung zur Sammlung Sultan</h1>
+    <h2>Data Inspector</h2>
+    <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error }}</div>
+      <ul v-else>
+        <li v-for="item in items" :key="item.id">
+          <pre>{{ item }}</pre>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
+
+
