f9eb8b8366c76bfcaca8d35c2cbe32387695c89f
culturetogo <m.mueller@culture-to-go.de>
2024-12-12 09:41:32 +0100

    adapted vite.config.js for github deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-deployment/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
