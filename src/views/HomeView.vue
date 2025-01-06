ab782ac48631a4cba67de2cc8a70adac2a3ebe24
culturetogo <m.mueller@culture-to-go.de>
2025-01-06 17:43:15 +0100

    feature: adapted for wikidata query

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,288 +1,111 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed } from 'vue';
 
 const itemsData = ref([]);
-const loading = ref(true);
+const rawData = ref(null);
+const items = ref([]);
+const loading = ref(false);
 const error = ref(null);
-
-const shortcuts = {
-  'culturalAssets': 'https://omeka-s-t1.berlin-university-collections.de/admin/item?sort_order=desc&resource_template_id%5B%5D=6&numeric%5Bts%5D%5Bgte%5D%5Bpid%5D=&numeric%5Bts%5D%5Bgte%5D%5Bval%5D=&year=&month=&day=&hour=&minute=&second=&offset=&numeric%5Bts%5D%5Blte%5D%5Bpid%5D=&numeric%5Bts%5D%5Blte%5D%5Bval%5D=&year=&month=&day=&hour=&minute=&second=&offset=&numeric%5Bdur%5D%5Bgt%5D%5Bpid%5D=&numeric%5Bdur%5D%5Bgt%5D%5Bval%5D=&years=&months=&days=&hours=&minutes=&seconds=&numeric%5Bdur%5D%5Blt%5D%5Bpid%5D=&numeric%5Bdur%5D%5Blt%5D%5Bval%5D=&years=&months=&days=&hours=&minutes=&seconds=&numeric%5Bivl%5D%5Bpid%5D=&numeric%5Bivl%5D%5Bval%5D=&year=&month=&day=&hour=&minute=&second=&offset=&numeric%5Bint%5D%5Bgt%5D%5Bpid%5D=&numeric%5Bint%5D%5Bgt%5D%5Bval%5D=&integer=&numeric%5Bint%5D%5Blt%5D%5Bpid%5D=&numeric%5Bint%5D%5Blt%5D%5Bval%5D=&integer=',
-}
-
-const items = computed(() => {
-  return itemsData.value
-  .filter(item => item['o:resource_template']['o:id'] === 6 )
-  .map(item => {
-      let mappedItem = {};
-      mappedItem['id'] = item['o:id'];
-      mappedItem['label'] = item['o:title'];
-      const provenanceProps = Object.keys(item).filter(key => key.startsWith('pro:'));
-      provenanceProps.forEach(prop => {
-        mappedItem[prop] = item[prop];
-      });
-      mappedItem['related'] = getRelatedInformationUnits(item['o:id']);
-      // mappedItem['provenanceStations'] = getProvenanceStations(item['o:id']);
-      mappedItem['provenanceStations'] =getProvenanceStations(item['o:id']);
-      return mappedItem;
-  })
-  .sort((a, b) => a.label.localeCompare(b.label));
-});
-
-function getSortDate(station) {
-  let date = '';
-  if (station['pro:date'] && station['pro:date'][0]) {
-    date = station['pro:date'][0]['@value'];
-  } else if (station['pro:dateLatest'] && station['pro:dateLatest'][0]) {
-    date = station['pro:dateLatest'][0]['@value'];
-  }
-  return date;
-}
-
-function getOwner(id) {
-  const source = itemsData.value
-    .find(item => item['o:id'] === id);
-  if (!source) {
-    return 'Owner not found';
-  }
-  // return source['pro:owner'][0]['@value'];
-  return source['pro:caOwner'] && source['pro:caOwner'][0]
-    ? source['pro:caOwner'][0]['@value']
-    : 'Owner not found';
-}
-
-function getStationInfo(station) {
-  let info = {
-    'id': station['o:id'],
-    'dbLink': getDBLink(station['o:id']),
-    'infoSourceDate': station['pro:dateDisplay'][0]['@value'],
-    'owner': getOwner(station['pro:infoSource'][0]['value_resource_id']),
-  };
-  return info;
-}
-
-function getProvenanceStations(id) {
-  let stations = itemsData.value
-    .filter(item => item['o:resource_template']['o:id'] === 8)
-    .filter(item => item['pro:relatesToCulturalAsset'][0]['value_resource_id'] === id)
-  if (!stations.length) {
-    return 'No provenance stations found';
-  }
-  let mappedStations = stations.map(station => {
-    return {
-      'label': station['dcterms:title'][0]['@value'],
-      'dbLink': getDBLink(station['o:id']),
-      'sortDate': getSortDate(station),
-      'stationInfo': getStationInfo(station),
-    }
-  });
-    return mappedStations;
-}
-
-function getRelatedInformationUnits(id) {
-  let relatedItems = itemsData.value
-    .filter(item => item['o:resource_template']['o:id'] === 7)
-    .filter(item => item['pro:relatesToCulturalAsset'][0]['value_resource_id'] === id)
-    .map(item => {
-      let mappedItem = {};
-      mappedItem['id'] = item['o:id'];
-      mappedItem['dbLInk'] = getDBLink(item['o:id']);
-      mappedItem['infoSourceDate'] = item['pro:infoSourceDate'][0]['@value'];
-      mappedItem['label'] = item['o:title'];
-      const provenanceProps = Object.keys(item).filter(key => key.startsWith('pro:'));
-      mappedItem['claims'] = provenanceProps
-        .filter(prop => prop.startsWith('pro:ca'))
-        .map(prop => {
-          return {
-            label: item[prop][0]['property_label'],
-            value: item[prop][0]['@value'],
-          };
-        });
-      return mappedItem;
-    })
-    .sort((a, b) => a.infoSourceDate.localeCompare(b.infoSourceDate));
-  let claims = {};
-  for (let item of relatedItems) {
-    for (let claim of item.claims) {
-      if (!claims[claim.label]) {
-        claims[claim.label] = [];
-      }
-      claims[claim.label].push({
-        value: claim.value,
-        infoSourceDate: item.infoSourceDate,
-        dbLink: item.dbLInk,
-      }) ;
-    }
-  }
-  for (let claim in claims) {
-    claims[claim].sort((a, b) => a.infoSourceDate.localeCompare(b.infoSourceDate));
-  }
-
-  return claims
-  // return relatedItems;
-}
-// extract value for specific property from related resource
-function getRelatedValue(prop, resourceId) {
-  const relatedItem = itemsData.value.find(item => item['o:id'] === resourceId);
-  if (!relatedItem) {
-    return 'Related item not found';
-  }
-  if (!relatedItem[prop] || !relatedItem[prop][0]) {
-    return 'Value for property not found';
-  }
-  return relatedItem[prop][0]['@value'];
-}
-
-function getDBLink(id) {
-  return `https://omeka-s-t1.berlin-university-collections.de/admin/item/${id}`;
-}
 const currentInput = ref('');
-const suggestions = computed(() => {
-  if (!currentInput.value || currentInput.value.length < 2) {
-    return [];
-  }
-  return itemsData.value
-    .filter(item => item['@type'].includes('pro:CulturalAsset'))
-    .filter(item => item['o:title'].toLowerCase().includes(currentInput.value.toLowerCase()))
-    .map(item => {
-      return {
-        label: item['o:title'],
-        id: item['o:id'],
-      };
-    });
-});
 
-const selectedItem = ref(null);
-function selectItem(id) {
-  const item = items.value.find(item => item.id === id);
-  if (item) {
-    selectedItem.value = item; // Set the selected item
-    currentInput.value = ''; // Clear the input
-  }
-}
-function getIllustrations(id) {
-  let illustrations = itemsData.value
-    .filter(item => item['o:resource_template']['o:id'] === 9)
-    .filter(item => item['pro:relatesToCulturalAsset'][0]['value_resource_id'] === id)
-    .map(item => {
-      return {
-        'label': item['dcterms:title'][0]['@value'],
-        'dbLink': getDBLink(item['o:id']),
-        'url': item['thumbnail_display_urls']['large'],
+async function submitSearch() {
+  if (currentInput.value.length) {
+    loading.value = true;
+    const query = `
+      SELECT DISTINCT ?artwork ?artworkLabel ?artworkTypeLabel ?image WHERE {
+      VALUES ?artist { wd:${currentInput.value} }  # Define the artist here (e.g., Leonardo da Vinci = Q762)
+    
+      ?artwork wdt:P170 ?artist .  # Filter works created by the artist
+      ?artwork wdt:P31 ?artworkType .
+    
+      OPTIONAL { ?artwork wdt:P18 ?image . }  # Include image if available
+    
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],de,en". }
       }
-    });
-  return illustrations;
-}
-onMounted(async () => {
-  try {
-    const response = await fetch('https://omeka-s-t1.berlin-university-collections.de/api/items');
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    `;
+    const url = `https://query.wikidata.org/sparql?query=${encodeURIComponent(
+        query
+      )}`;
+    console.log(url);
+    try {
+      const response = await fetch(url, {
+        headers: { Accept: "application/json" },
+      });
+      const data = await response.json();
+      rawData.value = data;
+
+      // itemsData.value = data.results;
+      itemsData.value = data.results.bindings.map((item) => ({
+        artwork: item.artwork.value,
+        artworkLabel: item.artworkLabel.value,
+        artworkTypeLabel: item.artworkTypeLabel.value,
+        image: item.image?.value || null,
+      }));
+    } catch (err) {
+      itemsData.value = [];
+      error.value = "Failed to fetch data. Please try again.";
+    } finally {
+      loading.value = false;
     }
-    itemsData.value = await response.json();
-  } catch (err) {
-    error.value = err instanceof Error ? err.message : 'Unknown error';
-  } finally {
-    loading.value = false;
   }
-});
+} 
+
 </script>
 
 <template>
   <main>
     <div class="headings content-element">
-      <h1>Provenienzforschung zur Sammlung Sultan</h1>
-      <h2>Data Inspector</h2>
+      <h1>Artworks on Wikidata</h1>
+      <h2>Lookup By Creator</h2>
     </div>
     <div class="controls content-element">
-      <div class="shortcuts">
-        <a :href="shortcuts.culturalAssets" target="_blank" rel="noopener">Werke in der Datenbank<img src="@/assets/icons/edit.svg" alt="Edit" class="icon edit-icon"/></a>
-      </div>
       <div class="search-control">
         <input type="text" class="search" v-model="currentInput" 
-          placeholder="Werk suchen..."
+          placeholder="Wikidata ID (Q-Number)"
         />
+        <button @click="submitSearch()" v-if="currentInput.length">Submit</button>
         <img src="@/assets/icons/x-circle.svg" alt="Edit" class="icon close-icon"
           @click="currentInput = ''"
           v-if="currentInput"
         />
       </div>
-      <div class="suggestions" v-if="currentInput && suggestions?.length">
-        <div class="suggestion"
-          v-for="item in suggestions"
-          :key="item.label"
-          @click="selectItem(item.id)"
-        >
-          {{ item.label }}
-        </div>
-      </div>
     </div>
-    <div class="display">
+    <div class="display content-element">
       <div v-if="loading">Loading...</div>
       <div v-else-if="error">{{ error }}</div>
       <div v-else>
-        <div class="item content-element" v-if="selectedItem">
-          <h3>{{ selectedItem.label }} <a :href="getDBLink(selectedItem.id)" target="_blank" rel="noopener">
-              <img src="@/assets/icons/edit.svg" alt="Edit" class="icon edit-icon"/>
-            </a>
-          </h3>
-          <div class="content">
-            <template v-if="selectedItem?.related">
-              <div class="field"
-                v-for="(values, label) in selectedItem.related"
-                :key="label"
-              >
-                <strong>{{ label }}</strong>
-                <div class="value"
-                  v-for="value in values"
-                  :key="value.value"
-                >
-                  {{ value.value }}
-                  <a :href="value.dbLink" target="_blank" rel="noopener">
-                    <img src="@/assets/icons/edit.svg" alt="Edit" class="icon edit-icon"/>
-                  </a>
-                </div>
-              </div>
-            </template>
-          </div>
-          <div class="provenance-stations">
-            <h4>Provenienzkette</h4>
-            <div v-if="selectedItem.provenanceStations === 'No provenance stations found'">
-              {{ selectedItem.provenanceStations }}
-            </div>
-            <div v-else>
-              <div class="station value"
-                v-for="station in selectedItem.provenanceStations"
-                :key="station.label"
-              >
-                {{ station.stationInfo.infoSourceDate }}: {{ station.stationInfo.owner }}
-                <a :href="station.stationInfo.dbLink" target="_blank" rel="noopener">
-                    <img src="@/assets/icons/edit.svg" alt="Edit" class="icon edit-icon"/>
-                  </a>
-              </div>
-            </div>
-          </div>
-          <div class="illustration" v-if="getIllustrations(selectItem.id)">
-            <h4>Bildquellen</h4>
-            <div class="thumbs">
-              <div class="thumb"
-                v-for="(illustration, index) in getIllustrations(selectedItem.id)"
-                :key="`thumb-${index}`"
-              >
-                <img :src="illustration.url" alt="Illustration" />
-                <a :href="illustration.dbLink" target="_blank" rel="noopener">
-                  <img src="@/assets/icons/edit.svg" alt="Edit" class="icon edit-icon"/>
-                </a>
-              </div>
-            </div>
-            <pre v-if="false">
-              
-              {{ getIllustrations(selectedItem.id) }}</pre>
-          </div>
-        </div>
+        <div class="table-container">
+    <table class="data-table">
+      <thead>
+        <tr>
+          <th>Label</th>
+          <th>Type</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in itemsData" :key="index">
+          <td>
+            <a :href="item.artwork" target="_blank">{{ item.artworkLabel }}</a></td>
+          <td>{{ item.artworkTypeLabel }}</td>
+          <td class="image-cell">
+            <img
+              v-if="item.image"
+              :src="item.image"
+              :alt="item.artworkLabel"
+            />
+            <span v-else>No Image</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
       </div>
   </div>
-  <div class="data-output" v-if="false">
-    <pre>{{ items }}</pre>
+  <div class="data-output" v-if="true">
+    <h3>Raw Data</h3>
+    <pre>{{ rawData }}</pre>
+    <pre>{{ itemsData }}</pre>
   </div>
 </main>
 </template>
@@ -387,6 +210,44 @@ h4 {
 }
 .thumb {
   margin-bottom: 1rem;
+}
+
+.table-container {
+  overflow-x: auto;
+  padding: 16px;
+}
+
+.data-table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+
+.data-table th,
+.data-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.data-table thead tr {
+  background-color: #f9f9f9;
+}
+
+.data-table tbody tr:nth-child(even) {
+  background-color: #f3f3f3;
+}
+
+.data-table .image-cell {
+  text-align: center;
+}
+
+.data-table .image-cell img {
+  max-width: 600px;
+  max-height: 400px;
+  height: auto;
+  display: block;
+  margin: auto;
 }
 </style>
 
